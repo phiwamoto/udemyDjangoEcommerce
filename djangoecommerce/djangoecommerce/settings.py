e3996df87c420e56fd1c0e9188e1103f586f1bfc
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2018-06-15 08:36:00 -0300

    Force deploy

--- djangoecommerce/djangoecommerce/settings.py
+++ djangoecommerce/djangoecommerce/settings.py
@@ -132,7 +132,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
